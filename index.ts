9abc7fe65423e370936401218a714c7e95e6c7d5
mh7434056859-collab <mh7434056859@gmail.com>
2026-04-17 14:39:47 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,13 +1,4 @@
-import express from "express";
-import cors from "cors";
-import router from "./app";
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.use("/", router);
+import app from "./app";
 
 const PORT = process.env.PORT || 3000;
 
